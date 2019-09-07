5d2428ec78580b6f44214b45e8bc4160ef43ceb0
miyass <exe.miyakura0629@gmail.com>
2019-09-07 19:17:04 +0900

    create table data

--- src/js/config.js
+++ src/js/config.js
@@ -1,24 +1,104 @@
 jQuery.noConflict();
 
-(function($, PLUGIN_ID) {
+(($, PLUGIN_ID) => {
   'use strict';
 
-  var $form = $('.js-submit-settings');
-  var $cancelButton = $('.js-cancel-button');
-  var $message = $('.js-text-message');
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+  const tableData = { 
+    editor: {
+      items: []
+    },
+    preview: {
+      items: []
+    },
+    fileLink: {
+      items: []
+    },
+    colorScheme: { value: '#ffffff' }
+  };
 
-  if (config.message) {
-    $message.val(config.message);
-  }
-  $form.on('submit', function(e) {
-    e.preventDefault();
-    kintone.plugin.app.setConfig({message: $message.val()}, function() {
-      alert('The plug-in settings have been saved. Please update the app!');
-      window.location.href = '/k/admin/app/flow?app=' + kintone.app.getId();
+  const createUITable = () => {
+    const table =  new kintoneUIComponent.Table({
+      data: [],
+      actionButtonsShown: false,
+      columns: [
+        {
+          header: 'Markdownエディタ',
+          cell: () => { return kintoneUIComponent.createTableCell('dropdown', 'editor')}
+        },
+        {
+          header: 'プレビューボタンの表示',
+          cell: () => { return kintoneUIComponent.createTableCell('dropdown', 'preview')}  
+        },
+        {
+          header: 'ファイルのリンク表示',
+          cell: () => { return kintoneUIComponent.createTableCell('dropdown', 'fileLink')}
+        },
+        {
+          header: '見出しタグの色*',
+          cell: () => { return kintoneUIComponent.createTableCell('text', 'colorScheme')}
+        }
+      ]
     });
-  });
-  $cancelButton.on('click', function() {
-    window.location.href = '/k/admin/app/' + kintone.app.getId() + '/plugin/';
+    return table;
+  }
+
+  const configElement = document.getElementById('setting');
+  const tableElement = createUITable();
+  
+  configElement.appendChild(tableElement.render());
+
+  const appId = kintone.app.getId();
+  let multiLineTextArray = [];
+  let spaceIdArray = [];
+  let tableDatas = [];
+
+  kintone.api(kintone.api.url('/k/v1/form', true), 'GET', {'app': appId}, (resp) => {
+    resp.properties.forEach(field => {
+      switch (field.type) {
+        case 'MULTI_LINE_TEXT':
+          multiLineTextArray.push(field.label);
+          break;
+        case 'SPACER':
+          spaceIdArray.push(field.elementId);
+          break;
+        default:
+          break;
+      }
+    });
+    
+    for (let i = 0; i <= 4; i++) {
+      const originTableData = JSON.parse(JSON.stringify(tableData));
+      const noSelectedItem = {
+        label: '-----',
+        value: '-----'
+      };
+      originTableData.editor.items.push(noSelectedItem);
+      originTableData.preview.items.push(noSelectedItem);
+      originTableData.fileLink.items.push(noSelectedItem);
+      multiLineTextArray.forEach((multiLineText, index) => {
+        const editorItem = {};
+        editorItem.label = multiLineText;
+        editorItem.value = multiLineText;
+        if(index === 0) {
+          originTableData.editor.value = multiLineText; 
+        }
+        originTableData.editor.items.push(editorItem);
+      });
+
+      spaceIdArray.forEach((spaceId, index) => {
+        const spaceIdItem = {};
+        spaceIdItem.label = spaceId;
+        spaceIdItem.value = spaceId;
+        if(index === 0) {
+          originTableData.preview.value = spaceId; 
+          originTableData.fileLink.value = spaceId; 
+        }
+        originTableData.preview.items.push(spaceIdItem);
+        originTableData.fileLink.items.push(spaceIdItem);
+      });
+
+      tableDatas.push(originTableData);
+    }
+    tableElement.setValue(tableDatas);
   });
 })(jQuery, kintone.$PLUGIN_ID);
